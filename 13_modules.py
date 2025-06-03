277849aa91453800d25864fde755389f649ce3c7
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2025-06-02 23:55:23 -0300

    modules

---
+++ 13_modules.py
@@ -0,0 +1,21 @@
+### Modules
+
+import module
+
+module.sumValue(5, 3, 7)
+module.printValue('Hola Python')
+
+
+from module import sumValue, printValue
+
+sumValue(5, 3, 7)
+printValue('Hola Python')
+
+import math
+
+print(math.pi)
+print(math.pow(2, 6))
+
+from math import pi as PI_VALUE
+
+print(PI_VALUE)
---
+++ module.py
@@ -0,0 +1,6 @@
+
+def sumValue(number_une, number_two, number_three):
+    print( number_une + number_two + number_three)
+
+def printValue(value):
+    print(value)   
