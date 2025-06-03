d9246093a8bd948553d8f17cf4bef61fb047bef5
RaulREscobar <raul.r.escobar@hotmail.com.ar>
2025-06-02 22:49:57 -0300

    classes

--- 11_classes.py
+++ 11_classes.py
@@ -8,9 +8,16 @@ print(MyEmptyPerson)
 
 class Person:
     def __init__(self, name, surname, alias = "Sin alias"):
-        self.full_name = f'{name} {surname} ({alias})'
-        self.name = name
-        self.surname = surname
+        self.full_name = f'{name} {surname} ({alias})' #Propiedad publica
+        self.__name = name #Propiedad Privada
+        self.__surname = surname
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_surname(self):
+        return self.__surname
+    
     def walk (self): 
         print(f'{self.full_name} está caminando')
 
@@ -26,4 +33,9 @@ print(my_other_person.full_name)
 my_other_person.walk()
 
 my_other_person.full_name = 'Juan Carlos (El Gaucho)'
+print(my_other_person.full_name)
+print(my_other_person.get_name())
+print(my_other_person.get_surname())
+
+my_other_person.full_name = 666
 print(my_other_person.full_name)
